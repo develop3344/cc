28bad3edb1fa7e3f58c9212f6484520a5fafa806
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-18 08:37:19 +0800

    Update 1112work3.c

--- 1112work3.c
+++ 1112work3.c
@@ -15,3 +15,21 @@ int main (void)
     printf("Sum is %d\n",sum);
     return 0;
 }
+
+
+# include <stdio.h>
+int main (void)
+{
+    int n,sum=0,term=1;
+
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    
+    for (int i=1;i<=n;i++)
+    {
+        term *= i; /*注意 += -= *=  /=  %= 的应用 */
+        sum += term;
+    }
+    printf("sum is %d\n",sum);
+    return 0;
+}
