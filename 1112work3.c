bdeb53d3e241200f0061df10060d2b364af3a6e0
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-18 08:11:52 +0800

    Update 1112work3.c

--- 1112work3.c
+++ 1112work3.c
@@ -1,8 +1,8 @@
 /*求1的阶乘 加到 n的阶乘 */
-#include<stdio.h>
+#include <stdio.h>
 int main (void)
 {
-    int i,n,s,sum;
+    int i,n,s,sum;/*i 表示循环变量 n上限值，循环次数？见课本P32 */
     s=1;
     sum=0;
     printf("Enter n:\n");
