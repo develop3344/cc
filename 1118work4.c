b14e53fcc5de2b9d34237f63eba6860c6e72b434
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-19 16:44:33 +0800

    Update 1118work4.c

--- 1118work4.c
+++ 1118work4.c
@@ -4,7 +4,6 @@ int main (void)
 {
     int n=1,fac=1;
     double e=1,term=1;
-    
 
     while (term>=1e-5)  /*表示 1乘以10的5次幂 */
     {
@@ -16,6 +15,9 @@ int main (void)
     printf("e is %lf\n",e);
     return 0;
 }
+
+
+
 
 
 /*求 e 的值 e= 1+ 1/1! +1/2! + ... +1/n!*/
