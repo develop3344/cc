d7c4902a5ea97219bf7acc074aa60396a92fef6c
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-19 09:32:43 +0800

    Create 1118work4.c

---
+++ 1118work4.c
@@ -0,0 +1,18 @@
+/*求 e 的值 */
+# include<stdio.h>
+int main (void)
+{
+    int n=1,fac=1;
+    double e=1,term=1;
+    
+
+    while (term>=10^-5)
+    {
+        fac=fac*n;
+        term=1.0/fac;
+        e+=term;
+        n=n+1;
+    }
+    printf("e is %lf\n",e);
+    return 0;
+}
