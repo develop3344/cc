b4c05422d9839ce64357f354bdf7ef8d9f957413
develop3344 <56298005+develop3344@users.noreply.github.com>
2019-11-26 12:33:34 +0800

    Create 20191125work6.c

---
+++ 20191125work6.c
@@ -0,0 +1,29 @@
+# include<stdio.h>
+# include<math.h>
+int main(void)
+{
+    int n,i,count=0;
+    for(n=1;n<=500;n++)
+    {
+        for(i=2;i<=sqrt(n);i++)
+        {
+            if(n%i==0)
+            {
+                break;
+            }
+        }
+        if(i>sqrt(n)) /*最后一步 执行了 i++ */
+        {
+            printf(" %d ",n); /*注意 引号与 %d 之间的空格 */
+            count++;          /*注意 count  */
+        }
+         if (  count%10 == 0 ) 
+
+        {
+
+            printf("\n"); /*输出结果 每行十项个数字*/
+
+        }
+    }
+    return 0;
+}
