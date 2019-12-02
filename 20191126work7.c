5fee9f5f6cda67d3b5954cfcaec6fc08fde4d9dc
develop37 <56298005+develop37@users.noreply.github.com>
2019-12-02 08:45:36 +0800

    Update 20191126work7.c

--- 20191126work7.c
+++ 20191126work7.c
@@ -7,9 +7,9 @@ int main(void)
     {
         for (y = 0; y <= 33; y++) //母鸡
         {
-            for (z = 0; z <= 99; z += 3) //小鸡只数 z 应为 3 的倍数因为钱数为整数，z+=1结果同
+            for (z = 0; z <= 99; z += 3) //小鸡只数 z 应为 3 的倍数因为钱数为整数  z++&&z/3.0等价z+3&&z/3.0
             {
-                if ((x + y + z == 100) && (5 * x + 3 * y + z / 3.0 == 100))
+                if ((x + y + z == 100) && (5 * x + 3 * y + z / 3== 100))
                 {
                     printf("公鸡 %d 只，母鸡 %d 只，小鸡 %d 只\n", x, y, z);
                     count ++;
@@ -22,3 +22,23 @@ return 0;
 }
 
 
+
+
+#include <stdio.h>
+int main(void)
+{
+    int cock,hen,chicken;
+    for( cock=0;cock<=20;cock++ )
+    {
+        for(hen=0;hen<=33;hen++)
+        {
+            chicken=100-cock-hen;              //重点
+            if(5*cock+3*hen+chicken/3.0 == 100)   //重点 15*cock+9*hen+chicken == 300   chicken%3==0
+            {
+                printf("cock: %d ,hen: %d ,chicken: %d \n",cock,hen,chicken);
+            }
+            
+        }
+    }
+    retuen 0;
+}
